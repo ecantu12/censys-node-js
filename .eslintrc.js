97f6eb5efcc0b890e028a5d4e31abdd0818f02d0
Aidan Holland <aidan@censys.io>
2021-04-29 16:41:34 -0400

    chore: add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2020": true,
-        "node": true,
-        "jest/globals": true
-    },
-    "extends": ["eslint:recommended", "plugin:jest/recommended"],
-    "parserOptions": {
-        "ecmaVersion": 11
-    },
-    "rules": {
-    },
-    "plugins": ["jest"]
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+    node: true,
+    "jest/globals": true,
+  },
+  extends: ["eslint:recommended", "plugin:jest/recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
+  plugins: ["jest"],
 };
