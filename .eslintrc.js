c397a385cd83659639542036d4883457f35f4a21
Aidan Holland <aidan@censys.io>
2021-05-06 17:58:37 -0400

    chore: enforce requires

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,20 @@ module.exports = {
     node: true,
     "jest/globals": true,
   },
-  extends: ["eslint:recommended", "plugin:jest/recommended", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "plugin:node/recommended",
+  ],
   parserOptions: {
-    ecmaVersion: 11,
+    ecmaVersion: 2020,
   },
   rules: {
     semi: ["error", "always"],
     quotes: ["error", "double"],
+    "node/global-require": "error",
+    "node/exports-style": ["error", "module.exports"],
   },
   plugins: ["jest"],
 };
