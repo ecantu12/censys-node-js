e5f12bea13127d88ad4e85bd37178a94031df0ce
Aidan Holland <aidan@censys.io>
2021-05-06 19:17:03 -0400

    feat: subdomains

--- src/asm/assets/domains.js
+++ src/asm/assets/domains.js
@@ -4,6 +4,14 @@ class DomainsAssets extends Assets {
   constructor(...args) {
     super("domains", ...args);
   }
+
+  async *getSubdomains(domain, pageNumber = 1, pageSize = null) {
+    yield* this.getPage(
+      `${this.basePath}/${domain}/subdomains`,
+      pageNumber,
+      pageSize
+    );
+  }
 }
 
 module.exports = DomainsAssets;
